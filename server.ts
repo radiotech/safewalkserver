4491bb8832784662267ad939609a98c84f6984a1
Andrew Harvey PC <ahharvey@live.unc.edu>
2018-01-12 21:40:35 -0500

    try again..

--- server.ts
+++ server.ts
@@ -1,20 +1,33 @@
-//Openshift variables
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "192.168.240.140";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+var WebSocketServer = require('ws').Server
+var http = require('http');
 
-//NodeJS require modules
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({host:ipaddress, port:8000});
-//var fs = require('fs');
-
-
-wss.on('connection', function(ws) {
-    console.log((new Date()) + ' Connection from origin: ' + ws._socket.remoteAddress);
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	  response.write("Welcome to Node.js on OpenShift!\n\n");
+	  response.end("Thanks for visiting us! \n");
 });
 
-console.log((new Date()) + " Server is listening on: " + ipaddress + ':' port);
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
 
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
 /*
 const express = require('express');
 const app = express();
